07492f19f8a06dbb73bb8b9a67eb035f9abb63c9
Jakenbake20 <jacob_wheat@yahoo.com>
2020-04-10 21:03:24 -0600

    added firebase to js and tags to index.

---
+++ assets/app.js
@@ -0,0 +1,14 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBEYpJHJBkRq2gmG9xEwYPSZmeX9mUpUNo",
+    authDomain: "jtwheat-b989b.firebaseapp.com",
+    databaseURL: "https://jtwheat-b989b.firebaseio.com",
+    projectId: "jtwheat-b989b",
+    storageBucket: "jtwheat-b989b.appspot.com",
+    messagingSenderId: "900933981143",
+    appId: "1:900933981143:web:612ee4f1a1a0486dcb3192",
+    measurementId: "G-D2H1J8KEK5"
+  };
+
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+    firebase.analytics();
